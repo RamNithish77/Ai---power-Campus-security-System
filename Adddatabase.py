2226d5921b98c58ce35763902c84ae56383a7ac0
Sriram <136118844+RamNithish77@users.noreply.github.com>
2024-11-15 16:17:08 +0530

    Add files via upload

---
+++ Adddatabase.py
@@ -0,0 +1,106 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL': ""
+})
+
+ref = db.reference('Students')
+
+data = {
+    "00001":
+        {
+            "name": "allu arjun",
+            "major": "Robotics",
+            "starting_year": 2017,
+            "total_attendance": 7,
+            "standing": "G",
+            "year": 4,
+            "last_attendance_time": "2022-12-11 00:54:34"
+        },
+    "00002":
+        {
+            "name": "mahesh",
+            "major": "Economics",
+            "starting_year": 2021,
+            "total_attendance": 12,
+            "standing": "B",
+            "year": 1,
+            "last_attendance_time": "2022-12-11 00:54:34"
+        },
+    "00003":
+        {
+            "name": "ntr",
+            "major": "Physics",
+            "starting_year": 2020,
+            "total_attendance": 7,
+            "standing": "G",
+            "year": 2,
+            "last_attendance_time": "2022-12-11 00:54:34"
+        },
+    "00004":
+        {
+            "name": "pawan kalyan",
+            "major": "Physics",
+            "starting_year": 2020,
+            "total_attendance": 7,
+            "standing": "G",
+            "year": 2,
+            "last_attendance_time": "2022-12-11 00:54:34"
+        },
+    "00005":
+        {
+            "name": "prabas",
+            "major": "Physics",
+            "starting_year": 2020,
+            "total_attendance": 7,
+            "standing": "G",
+            "year": 2,
+            "last_attendance_time": "2022-12-11 00:54:34"
+        },
+    "00006":
+        {
+            "name": "ram charan",
+            "major": "Physics",
+            "starting_year": 2020,
+            "total_attendance": 7,
+            "standing": "G",
+            "year": 2,
+            "last_attendance_time": "2022-12-11 00:54:34"
+        },
+    "00007":
+        {
+            "name": "Ramu",
+            "major": "Physics",
+            "starting_year": 2020,
+            "total_attendance": 7,
+            "standing": "G",
+            "year": 2,
+            "last_attendance_time": "2022-12-11 00:54:34"
+        },
+        "00008":
+        {
+            "name": "siddi",
+            "major": "Robotics",
+            "starting_year": 2017,
+            "total_attendance": 7,
+            "standing": "G",
+            "year": 4,
+            "last_attendance_time": "2022-12-11 00:54:34"
+        },
+        "00009":
+        {
+            "name": "tharun",
+            "major": "Robotics",
+            "starting_year": 2017,
+            "total_attendance": 7,
+            "standing": "G",
+            "year": 4,
+            "last_attendance_time": "2022-12-11 00:54:34"
+        },
+}
+
+for key, value in data.items():
+    ref.child(key).set(value)
